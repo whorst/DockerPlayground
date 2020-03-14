8c649db41e857a43568980a6e5bad7871d5af5dc
whorst042 <whorst042@gmail.com>
2020-03-14 12:01:49 -0400

    dock networking

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,4 @@
-INSERT INTO users (username, password, enabled)
-    values(
-        'user',
-        'pass',
-        true);
-
-INSERT INTO users (username, password, enabled)
-    values(
-        'admin',
-        'pass',
-        true);
-
-INSERT INTO authorities (username, authority)
-    values ('user', 'ROLE_USER');
-INSERT INTO authorities (username, authority)
-    values ('admin', 'ROLE_ADMIN');
+INSERT INTO spring_security.users (username, password, enabled)values('user','pass',true);
+INSERT INTO spring_security.users (username, password, enabled)values('admin','pass',true);
+INSERT INTO spring_security.authorities (username, authority)values ('user', 'ROLE_USER');
+INSERT INTO spring_security.authorities (username, authority)values ('admin', 'ROLE_ADMIN');
